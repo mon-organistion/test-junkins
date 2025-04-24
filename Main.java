4c1897c478e474dd9ecbc36f758f46654ab80a04
mor mbathie <40276302+mormbathie@users.noreply.github.com>
2025-04-24 09:49:12 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello,Mor mathie!");
-        System.out.println("Diougeul dioulliji jottna deh ");
+        System.out.println("cv boy ");
     }
 }
